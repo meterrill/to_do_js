1539c8b53df5eb6f485f0306fc7bbd3ddf0ffc48
Frank Meza and Matthew Terrill <meterrill@gmail.com>
2015-02-19 15:46:17 -0800

    add ability to add tasks to lists

--- js/scripts.js
+++ js/scripts.js
@@ -2,27 +2,38 @@ $(function() {
   $("form#lists").submit(function(event) {
     event.preventDefault();
 
-    var newList = $("input#new-list").val();
-    var list = {list: newList, task: []};
+    var listName = $("input#new-list").val();
 
-    $("ul#list-names").append("<li><span class='active-list'>" + list.list + "</span></li>");
+    var newList = {list: listName, tasks: []};
+
+
+    $("form#tasks").submit(function(event) {
+      event.preventDefault();
+
+      var newTask = $("input#new-task").val();
+      var task = {description: newTask};
+      newList.tasks.push(task);
+
+      $("ul#to-do-tasks").text("");
+      newList.tasks.forEach(function(task) {
+        $("ul#to-do-tasks").append("<li><span class='active-task'>"+ task.description + "</span></li>");
+      });
+
+      $("input#new-task").val("");
+
+      // $("#to-do-tasks li").last().click(function() {
+      //   $(this).appendTo("ul#completed-tasks");
+      // });
+    });
+
+
+    $("ul#list-names").append("<li><span class='active-list'>" + newList.list + "</span></li>");
 
     $("input#new-list").val("");
-
   });
 
-  $("form#tasks").submit(function(event) {
-    event.preventDefault();
-
-    var newTask = $("input#new-task").val();
-    var task = {task: newTask};
-
-    $("ul#to-do-tasks").append("<li><span class='active-task'>"+ task.task + "</span></li>");
-
-    $("input#new-task").val("");
-
-    $("#to-do-tasks li").last().click(function() {
-      $(this).appendTo("ul#completed-tasks");
-    });
+  $(".active-list").click(function() {
+    $(this).show(newList.tasks)
   });
+
 });
