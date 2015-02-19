4ae89701ac2f1889077a3270ebe77eb2e3b031e6
Frank Meza and Matthew Terrill <fmeza07@gmail.com>
2015-02-19 12:16:22 -0800

    add ability to add mulitple lists

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
 $(function() {
+  $("form#lists").submit(function(event) {
+    event.preventDefault();
+
+    var newList = $("input#new-list").val();
+    var list = {list: newList, task: []};
+
+    $("ul#list-names").append("<li><span class='active-list'>" + list.list + "</span></li>");
+
+    $("input#new-list").val("");
+
+  });
+
   $("form#tasks").submit(function(event) {
     event.preventDefault();
 
@@ -6,7 +18,6 @@ $(function() {
     var task = {task: newTask};
 
     $("ul#to-do-tasks").append("<li><span class='active-task'>"+ task.task + "</span></li>");
-
 
     $("input#new-task").val("");
 
